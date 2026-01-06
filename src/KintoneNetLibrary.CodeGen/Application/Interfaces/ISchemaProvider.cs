3738b8386c33e03df43ed9ac1c36e1eddbcf5857
MORITA Ken-ichi <moriken@k14a.net>
2026-01-06 20:13:04 +0900

    コミット漏れ

--- src/KintoneNetLibrary.CodeGen/Application/Interfaces/ISchemaProvider.cs
+++ src/KintoneNetLibrary.CodeGen/Application/Interfaces/ISchemaProvider.cs
@@ -1,8 +1,10 @@
 using KintoneNetLibrary.CodeGen.Domain.Schemas;
+using KintoneNetLibrary.Domain.Entities;
 
 namespace KintoneNetLibrary.CodeGen.Application.Interfaces;
 
 public interface ISchemaProvider {
     Task<KintoneAppSchema> GetSchemaAsync(int appId, string apiToken);
     void SetDomain(string subDomain);
+    Task<KintoneAppMetadata> GetMetadataAsync(int appId, string apiToken)
 }
